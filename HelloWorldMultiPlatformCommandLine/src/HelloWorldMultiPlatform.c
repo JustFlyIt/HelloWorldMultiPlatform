35464ed195364ddc932d5c0f764a22e20d514a5a
Don Snyder <DonRSnyder@gmail.com>
2019-05-09 18:05:44 -0400

    Added commandline version

---
+++ HelloWorldMultiPlatformCommandLine/src/HelloWorldMultiPlatform.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : HelloWorldMultiPlatform.c
+ Author      : Don Snyder
+ Version     :
+ Copyright   :
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	puts("Hello World Multi-Platform"); /* prints Hello World Multi-Platform */
+	return EXIT_SUCCESS;
+
+}
