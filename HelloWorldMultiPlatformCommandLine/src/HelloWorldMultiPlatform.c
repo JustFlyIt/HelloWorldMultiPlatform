9dbbe6d9153fcb8d5712f86cfb06ec870d6a9632
Don Snyder <DonRSnyder@gmail.com>
2019-05-22 15:23:21 -0400

    Added modest functionality

--- HelloWorldMultiPlatformCommandLine/src/HelloWorldMultiPlatform.c
+++ HelloWorldMultiPlatformCommandLine/src/HelloWorldMultiPlatform.c
@@ -10,10 +10,22 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main(void) {
 
-	puts("Hello World Multi-Platform"); /* prints Hello World Multi-Platform */
+	puts("Hello World Multi-Platform\n\n"); /* prints Hello World Multi-Platform */
+
+	int y = 0;
+
+	printf("Enter an integer: ");
+	scanf("%d",&y);
+	printf("Entered Number = %d\n\n",y);
+
+	int x = sqrt(y);
+
+	printf("Square Root Input: %d\n\n", x);
+
 	return EXIT_SUCCESS;
 
 }
